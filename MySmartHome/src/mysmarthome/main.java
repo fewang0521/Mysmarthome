9ee7e9a52b986888e2e38d89d5e3a4a0ccb28e67
fewang0521 <36626179+fewang0521@users.noreply.github.com>
2018-03-06 20:44:26 +0900

    main change for args

--- MySmartHome/src/mysmarthome/main.java
+++ MySmartHome/src/mysmarthome/main.java
@@ -5,19 +5,43 @@ public class main {
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Serialfunction port =new Serialfunction();
-		Databasefunction db = new Databasefunction();
+		if ((args.length)==0){
+			System.out.println("please enter args");
+		}
+		else{
+			String parameter_name =args[0];
+			
+			switch (parameter_name) {
+			case "light": 
+				Serialfunction port =new Serialfunction();
+				Databasefunction db = new Databasefunction();
+				
 		
-
-		port.serialconnect("COM5");
-		port.commandsend('t');
-		port.commandreceive();
-		port.serialclose();
-		String value = new String(port.save,0,0,port.buffer_index-1);
-		int int_value = Integer.parseInt(value);
-		db.sql_run("temp", int_value, "succes");
-		System.exit(1);
-		
+				port.serialconnect("COM5");
+				port.commandsend('r');
+				port.commandreceive();
+				port.serialclose();
+				String value = new String(port.save,0,0,port.buffer_index-1);
+				int int_value = Integer.parseInt(value);
+				db.sql_run("light", 0, value);
+				System.exit(1);
+			}
+			
+			/*
+			Serialfunction port =new Serialfunction();
+			Databasefunction db = new Databasefunction();
+			
+	
+			port.serialconnect("COM5");
+			port.commandsend('r');
+			port.commandreceive();
+			port.serialclose();
+			String value = new String(port.save,0,0,port.buffer_index-1);
+			int int_value = Integer.parseInt(value);
+			db.sql_run("light", 0, value);
+			System.exit(1);
+			*/
+		}
 
 	}
 
