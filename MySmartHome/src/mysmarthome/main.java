cdea88704bae640fdcfddd56c4d06ac0bde31075
fewang0521 <36626179+fewang0521@users.noreply.github.com>
2018-03-06 21:02:06 +0900

    args change

--- MySmartHome/src/mysmarthome/main.java
+++ MySmartHome/src/mysmarthome/main.java
@@ -10,21 +10,45 @@ public class main {
 		}
 		else{
 			String parameter_name =args[0];
+			Serialfunction port =new Serialfunction();
+			Databasefunction db = new Databasefunction();
+			String value;
+			int int_value;
 			
 			switch (parameter_name) {
 			case "light": 
-				Serialfunction port =new Serialfunction();
-				Databasefunction db = new Databasefunction();
-				
-		
+					
 				port.serialconnect("COM5");
 				port.commandsend('r');
 				port.commandreceive();
 				port.serialclose();
-				String value = new String(port.save,0,0,port.buffer_index-1);
-				int int_value = Integer.parseInt(value);
+				value = new String(port.save,0,0,port.buffer_index-1);
+				//int int_value = Integer.parseInt(value);
 				db.sql_run("light", 0, value);
 				System.exit(1);
+				
+				
+			case "temp": 
+						
+				port.serialconnect("COM5");
+				port.commandsend('r');
+				port.commandreceive();
+				port.serialclose();
+				value = new String(port.save,0,0,port.buffer_index-1);
+				int_value = Integer.parseInt(value);
+				db.sql_run("temp", int_value, "sucess");
+				System.exit(1);
+				
+			case "humidity": 
+				
+				port.serialconnect("COM5");
+				port.commandsend('r');
+				port.commandreceive();
+				port.serialclose();
+				value = new String(port.save,0,0,port.buffer_index-1);
+				int_value = Integer.parseInt(value);
+				db.sql_run("humidity", int_value, "sucess");
+				System.exit(1);	
 			}
 			
 			/*
