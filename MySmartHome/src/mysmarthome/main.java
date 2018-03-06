6b3277e39b936efdf59c84175223ca74a408689a
fewang0521 <36626179+fewang0521@users.noreply.github.com>
2018-03-06 21:05:00 +0900

    args_delete /* sentence

--- MySmartHome/src/mysmarthome/main.java
+++ MySmartHome/src/mysmarthome/main.java
@@ -49,22 +49,7 @@ public class main {
 				int_value = Integer.parseInt(value);
 				db.sql_run("humidity", int_value, "sucess");
 				System.exit(1);	
-			}
-			
-			/*
-			Serialfunction port =new Serialfunction();
-			Databasefunction db = new Databasefunction();
-			
-	
-			port.serialconnect("COM5");
-			port.commandsend('r');
-			port.commandreceive();
-			port.serialclose();
-			String value = new String(port.save,0,0,port.buffer_index-1);
-			int int_value = Integer.parseInt(value);
-			db.sql_run("light", 0, value);
-			System.exit(1);
-			*/
+			}			
 		}
 
 	}
